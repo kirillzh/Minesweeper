bb8f96a9ec36c5090fa8c4d44ef976fc2f650d30
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-12-08 10:24:02 -0800

    Some test for board

--- app/src/androidTest/java/com/zhukov/minesweeper/ApplicationTest.java
+++ app/src/androidTest/java/com/zhukov/minesweeper/ApplicationTest.java
@@ -3,6 +3,7 @@ package com.zhukov.minesweeper;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
